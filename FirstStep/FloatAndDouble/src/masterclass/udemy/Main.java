bb7128646b754e2bd80a8ee2d24c585e98ab7ff7
Wei <wzheng100@gmail.com>
2021-07-10 12:49:23 -0700

    challenge

---
+++ FirstStep/FloatAndDouble/src/masterclass/udemy/Main.java
@@ -0,0 +1,19 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	float myMinFloatValue = Float.MIN_VALUE;
+	float myMaxFloatValue = Float.MAX_VALUE;
+
+	System.out.println("Float minimum value = " + myMinFloatValue);
+	System.out.println("Float maximum value = " + myMaxFloatValue);
+	
+	double myMinDoubleValue = Double.MIN_VALUE;
+	double myMaxDoubleValue = Double.MAX_VALUE;
+
+	System.out.println("Double minimum value = " + myMinDoubleValue);
+	System.out.println("Double maximum value = " + myMaxDoubleValue);
+    }
+}
