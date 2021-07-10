b4fcc03f6fbfb736c99c01f4c13c64cede32698e
Wei <wzheng100@gmail.com>
2021-07-10 15:38:34 -0700

    Char and Boolean

---
+++ FirstStep/CharAndBoolean/src/masterclass/udemy/Main.java
@@ -0,0 +1,21 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar= '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myUnicodeChar2= '\u00A9';
+        System.out.println(myUnicodeChar2);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
