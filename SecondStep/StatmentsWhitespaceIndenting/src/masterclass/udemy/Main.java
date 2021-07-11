4520ca2bf8df0742973a8cdb96fb89d9337e6a72
Wei <wzheng100@gmail.com>
2021-07-11 14:10:17 -0700

    White Space Indenting Statements

---
+++ SecondStep/StatmentsWhitespaceIndenting/src/masterclass/udemy/Main.java
@@ -0,0 +1,18 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;        //statement is the entire line datatype plus the expression and the semicolon
+        myVariable++;
+        myVariable--;
+        
+        System.out.println("This is a test"); //complete java statement
+        System.out.println("This is " +
+                "a " +
+                "test.");
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
+    }
+}
