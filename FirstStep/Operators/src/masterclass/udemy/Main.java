941fcc3517add1ade38d2d89bd91d628e1cc1e7a
Wei <wzheng100@gmail.com>
2021-07-10 17:46:30 -0700

    Operators

---
+++ FirstStep/Operators/src/masterclass/udemy/Main.java
@@ -0,0 +1,46 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	int result = 1 + 2; // 1 + 2 = 3
+    System.out.println("1 + 2 = " + result);
+    int previousResult = result;
+    System.out.println("previousResult = " + previousResult);
+    result = result - 1;    // 3 - 1
+    System.out.println("3 - 1 = " + result);
+    System.out.println("previousResult = " + previousResult);
+
+    result = result * 10;   // 2 * 10
+    System.out.println("2 * 10 = " + result);
+
+    result = result / 5;    //20 / 5 = 4
+    System.out.println("20 / 5 = " + result);
+
+    result = result % 3;    //4 % 3 = 1
+    System.out.println("4 % 3 = " + result);
+
+    //result = result + 1;
+    result++; // 1 + 1 = 2
+    System.out.println("1 + 1 = " + 2);
+
+    result--; // 2 - 1 = 2
+    System.out.println("2 - 1 = " + result);
+
+    // result = result + 2
+    result += 2; // 1 + 2 = 3'
+    System.out.println("1 + 2 = " + result);
+
+    // result = result * 10;
+    result *= 10; // 3 * 10
+    System.out.println("3 * 10 = " + result);
+
+    //result = result / 3
+    result /= 3;
+    System.out.println("30 / 3 = " + result);
+
+    result -= 2;
+    System.out.println("10 - 2 = " + result);
+    }
+}
