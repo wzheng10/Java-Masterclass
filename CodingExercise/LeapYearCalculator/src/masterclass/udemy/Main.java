ed785ca5c35fb6103f7303003e6fe5d6a6bd5427
Wei <wzheng100@gmail.com>
2021-07-12 12:06:45 -0700

    Leap Year Calculator

---
+++ CodingExercise/LeapYearCalculator/src/masterclass/udemy/Main.java
@@ -0,0 +1,14 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
