f6f6383fa2a7d607b2a96d657ac0d803b9194f3a
Wei <wzheng100@gmail.com>
2021-07-12 12:21:38 -0700

    EqualSumChecker

---
+++ CodingExercise/6_EqualSumChecker/src/masterclass/udemy/Main.java
@@ -0,0 +1,12 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
+        if ( num1 + num2 == num3){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
