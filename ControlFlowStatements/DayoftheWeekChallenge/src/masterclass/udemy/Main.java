49ff66be5fe7627e97b0e993af3567afccacb08f
Wei <wzheng100@gmail.com>
2021-07-13 08:20:09 -0700

    Challenge

---
+++ ControlFlowStatements/DayoftheWeekChallenge/src/masterclass/udemy/Main.java
@@ -0,0 +1,39 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(2);
+    }
+
+
+    public static void printDayOfTheWeek(int day) {
+       switch(day) {
+           case 0:
+               System.out.println("Sunday");
+               break;
+           case 1:
+               System.out.println("Monday");
+               break;
+           case 2:
+               System.out.println("Tuesday");
+               break;
+           case 3:
+               System.out.println("Wednesday");
+               break;
+           case 4:
+               System.out.println("Thursday");
+               break;
+           case 5:
+               System.out.println("Friday");
+               break;
+           case 6:
+               System.out.println("Saturday");
+               break;
+           default:
+               System.out.println("Invalid Day");
+               break;
+       }
+    }
+}
