4e3a547784592a27b370ae21b758a482f3043a47
Wei <wzheng100@gmail.com>
2021-07-12 22:11:29 -0700

    AreaCalculator

---
+++ CodingExercise/8_AreaCalculator/src/masterclass/udemy/Main.java
@@ -0,0 +1,18 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1;
+        }
+        return x * y;
+    }
+}
