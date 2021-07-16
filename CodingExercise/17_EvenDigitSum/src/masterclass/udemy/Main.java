81e38ff406fd6c0cd49c6b7ef3a9c538bb1cc70b
Wei <wzheng100@gmail.com>
2021-07-15 21:20:55 -0700

    Even Digit Sum

--- CodingExercise/17_EvenDigitSum/src/masterclass/udemy/Main.java
+++ CodingExercise/17_EvenDigitSum/src/masterclass/udemy/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(12));
     }
 
     public static int getEvenDigitSum(int number) {
