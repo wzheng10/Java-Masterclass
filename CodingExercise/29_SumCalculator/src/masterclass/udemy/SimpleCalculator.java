5ce9b5011102daaea7e29e8bfa45b5f6154f1480
Wei <wzheng100@gmail.com>
2021-07-18 22:17:26 -0700

    Sum Calculator

--- CodingExercise/29_SumCalculator/src/masterclass/udemy/SimpleCalculator.java
+++ CodingExercise/29_SumCalculator/src/masterclass/udemy/SimpleCalculator.java
@@ -1,2 +1,37 @@
-package masterclass.udemy;public class SimpleCalculator {
+package masterclass.udemy;
+
+public class SimpleCalculator {
+
+    private double firstNumber;
+    private double secondNumber;
+
+    public void setFirstNumber(double firstNumber){
+        this.firstNumber = firstNumber;
+    }
+    public void setSecondNumber(double secondNumber){
+        this.secondNumber = secondNumber;
+    }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0;
+        } else {
+            return firstNumber / secondNumber;
+        }
+    }
 }
