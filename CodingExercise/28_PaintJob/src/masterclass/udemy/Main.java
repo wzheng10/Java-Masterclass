8a3c5535a6e7a657ada4aea8eb4e51dbf71ed61a
Wei <wzheng100@gmail.com>
2021-07-18 19:49:13 -0700

    Paint Job

--- CodingExercise/28_PaintJob/src/masterclass/udemy/Main.java
+++ CodingExercise/28_PaintJob/src/masterclass/udemy/Main.java
@@ -6,41 +6,25 @@ public class Main {
         // write your code here
     }
 
-//    public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets) {
-//        if (width < 0 || height < 0 || areaPerBucket < 0 || extraBuckets < 0)
-//            return -1;
-//        return (int) Math.ceil((width * height - (areaPerBucket * extraBuckets)) / areaPerBucket);
-//    }
-//
-//    public static int getBucketCount(double width, double height, double areaPerBucket) {
-//        if (width < 0 || height < 0 || areaPerBucket < 0) {
-//            return -1;
-//        }
-//        return getBucketCount(width, height, areaPerBucket, 0);
-//
-//    }
-//
-//    public static int getBucketCount(double area, double areaPerBucket) {
-//        if (area <= 0 || areaPerBucket < 0)
-//            return -1;
-//        return (int) Math.ceil(area / areaPerBucket);
-//
-//    }
-//}
-
-public class PaintJob {
-    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
-        if(width<=0 || height<=0 || areaPerBucket<=0 || extraBuckets<0) return -1;
-        return (int) Math.ceil((width*height-(areaPerBucket*extraBuckets))/areaPerBucket);
+    public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets) {
+        if (width < 0 || height < 0 || areaPerBucket < 0 || extraBuckets < 0)
+            return -1;
+        return (int) Math.ceil((width * height - (areaPerBucket * extraBuckets)) / areaPerBucket);
     }
 
-    public static int getBucketCount(double width, double height, double areaPerBucket){
-        if(width<=0 || height<=0 || areaPerBucket<=0) return -1;
-        return (int) Math.ceil(width*height/areaPerBucket);
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width < 0 || height < 0 || areaPerBucket < 0) {
+            return -1;
+        }
+        return getBucketCount(width, height, areaPerBucket, 0);
+
     }
 
-    public static int getBucketCount(double area, double areaPerBucket){
-        if(area<=0 || areaPerBucket<=0) return -1;
-        return (int) Math.ceil(area/areaPerBucket);
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket < 0)
+            return -1;
+        return (int) Math.ceil(area / areaPerBucket);
+
     }
 }
+
