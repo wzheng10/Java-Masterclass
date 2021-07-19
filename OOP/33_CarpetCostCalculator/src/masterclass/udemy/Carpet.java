13345b787e9c133b7642fb9e2b7630078d9d5711
Wei <wzheng100@gmail.com>
2021-07-19 13:04:12 -0700

    Carpet Cost Calculator

---
+++ OOP/33_CarpetCostCalculator/src/masterclass/udemy/Carpet.java
@@ -0,0 +1,14 @@
+package masterclass.udemy;
+
+public class Carpet {
+
+    private double cost;
+
+    public Carpet( double cost) {
+        this.cost = cost < 0 ? 0 : cost;
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
+}
