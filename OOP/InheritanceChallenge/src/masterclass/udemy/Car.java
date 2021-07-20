8954b8a632c2eb11a5a019457b8030da09c05c8e
Wei <wzheng100@gmail.com>
2021-07-20 13:41:10 -0700

    Challenge

---
+++ OOP/InheritanceChallenge/src/masterclass/udemy/Car.java
@@ -0,0 +1,35 @@
+package masterclass.udemy;
+
+public class Car extends Vehicle {
+
+    private int wheels;
+    private int doors;
+    private int gear;
+    private boolean isManual;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gear, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gear = gear;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Change to " + this.currentGear + " gear.");
+    }
+    public void changeVelocity(int speed, int direction) {
+        System.out.println("Car.changeVelocity(): Velocity " + speed + " direction " + direction);
+        move(speed, direction);
+    }
+
+//    @Override
+//    public void stop() {
+//        super.stop();
+//    }
+}
+
