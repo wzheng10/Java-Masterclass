44b2624ab41d2d629e9407720ad68a711318def4
Wei <wzheng100@gmail.com>
2021-07-21 01:09:52 -0700

    Encapsulation Challenge

--- OOP_2/Encapsulation/src/masterclass/udemy/Main.java
+++ OOP_2/Encapsulation/src/masterclass/udemy/Main.java
@@ -16,10 +16,20 @@ public class Main {
 //        damage = 11;
 //        player.loseHealth(damage);
 //        System.out.println("Reamaining Health = " + player.healthRemaining());
-        EnhancedPlayer player = new EnhancedPlayer("Wei", 250, "Sword");
-        System.out.println("Initial health is "+ player.getHitPoints());
-        int damage = 50;
-        player.loseHealth(damage);
-        System.out.println("Remaining Health = " + player.getHitPoints());
+
+//        EnhancedPlayer player = new EnhancedPlayer("Wei", 250, "Sword");
+//        System.out.println("Initial health is "+ player.getHitPoints());
+//        int damage = 50;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining Health = " + player.getHitPoints());
+
+        Printer printer = new Printer(50, false);
+        System.out.println("Initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + "new total print count for printer = " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + "new total print count for printer = " + printer.getPagesPrinted());
+//        printer.addToner(20);
+//        System.out.println();
     }
 }
