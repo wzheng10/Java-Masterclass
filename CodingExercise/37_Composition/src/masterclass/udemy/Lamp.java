0f4567032096057dc41181ab2e803e33047a34a1
Wei <wzheng100@gmail.com>
2021-07-20 18:08:11 -0700

    Composition

---
+++ CodingExercise/37_Composition/src/masterclass/udemy/Lamp.java
@@ -0,0 +1,27 @@
+package masterclass.udemy;
+
+public class Lamp {
+    private String style;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(String style, boolean battery, int globRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+
+    public void turnOn() {
+        System.out.println("Lamp is turned on");
+    }
+
+    public String getStyle(){
+        return this.style;
+    }
+    public boolean isBattery() {
+        return battery;
+    }
+    public int getGlobRating() {
+        return globRating;
+    }
+}
