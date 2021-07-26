594de15de0dd192168be8b8794fe1d3ac07cbc6c
Wei <wzheng100@gmail.com>
2021-07-25 23:34:24 -0700

    Update Contacts Bug Fix

--- CodingExercise/44_MobilePhone/src/masterclass/udemy/MobilePhone.java
+++ CodingExercise/44_MobilePhone/src/masterclass/udemy/MobilePhone.java
@@ -26,12 +26,16 @@ public class MobilePhone {
         if (foundPosition < 0) {
             System.out.println(oldContact.getName() + ", was not found.");
             return false;
+        } else if (findContact(newContact.getName()) != -1) {
+            System.out.println("Contact with name " + newContact.getName() +
+                    " already exists. Update was not successful.");
+        return false;
         }
 
-        this.myContacts.set(foundPosition, newContact);
-        System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
+        this.myContacts.set(foundPosition,newContact);
+        System.out.println(oldContact.getName()+", was replaced with "+newContact.getName());
         return true;
-    }
+}
 
     public boolean removeContact(Contact contact) {
         int foundPosition = findContact(contact);
@@ -64,9 +68,10 @@ public class MobilePhone {
         }
         return null;
     }
+
     public Contact queryContact(String name) {
         int position = findContact(name);
-        if(position>=0) {
+        if (position >= 0) {
             return this.myContacts.get(position);
         }
 
