e989bed3c87e4223d0997b74ffd278336ad7a613
Wei <wzheng100@gmail.com>
2021-07-26 14:51:19 -0700

    Banking

---
+++ CodingExercise/45_Banking/src/masterclass/udemy/Branch.java
@@ -0,0 +1,56 @@
+package masterclass.udemy;
+
+import java.util.ArrayList;
+
+public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String customerName, double initialAmount) {
+        if (findCustomer(customerName) == null) {
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+        if (existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+
+
+    private Customer findCustomer(String customerName) {
+        for (int i = 0; i < this.customers.size(); i++) {
+            Customer checkedCustomer = this.customers.get(i);
+            if (checkedCustomer.getName().equals(customerName)) {
+//      this means this current instance of the given class(Bank class instance)
+//      customers is the ArrayList<Customer> field of Bank class(customer list of this branch.)
+//      get(i) returns the Customer whose index matches the current iteration of the loop.
+//      get(name) returns the name of that customer as a String.
+//      equals(customerName) returns true if the current customer name being looked at is equal to the customerName parameter, and false otherwise.
+                return checkedCustomer;
+            }
+        }
+        return null;
+    }
+}
