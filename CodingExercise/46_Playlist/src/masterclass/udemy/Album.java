24a9daefe5279b52cfcfc33261e48effbc056f5c
Wei <wzheng100@gmail.com>
2021-07-28 13:27:52 -0700

    Song Class

---
+++ CodingExercise/46_Playlist/src/masterclass/udemy/Album.java
@@ -0,0 +1,12 @@
+package masterclass.udemy;
+
+import java.util.ArrayList;
+
+public class Album {
+
+    private String name;
+    private String artist;
+    private ArrayList
+
+    private String findSong()
+}
