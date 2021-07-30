560793958b66c154e2982c3f11c32bfd468f8d36
Wei <wzheng100@gmail.com>
2021-07-30 14:09:31 -0700

    Removal

--- CodingExercise/49_AbstractClass/src/masterclass/udemy/Main.java
+++ CodingExercise/49_AbstractClass/src/masterclass/udemy/Main.java
@@ -15,24 +15,24 @@ public class Main {
         }
 
         tree.traverse(tree.getRoot());
-//        tree.removeItem(new Node("3"));
-//        tree.traverse(tree.getRoot());
-//
-//        tree.removeItem(new Node("5"));
-//        tree.traverse(tree.getRoot());
-//
-//        tree.removeItem(new Node("0"));
-//        tree.removeItem(new Node("4"));
-//        tree.removeItem(new Node("2"));
-//        tree.traverse(tree.getRoot());
-//
-//        tree.removeItem(new Node("9"));
-//        tree.traverse(tree.getRoot());
-//        tree.removeItem(new Node("8"));
-//        tree.traverse(tree.getRoot());
-//        tree.removeItem(new Node("6"));
-//        tree.traverse(tree.getRoot());
-//        tree.removeItem(tree.getRoot());
-//        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("3"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("5"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("0"));
+        tree.removeItem(new Node("4"));
+        tree.removeItem(new Node("2"));
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("9"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("8"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("6"));
+        tree.traverse(tree.getRoot());
+        tree.removeItem(tree.getRoot());
+        tree.traverse(tree.getRoot());
     }
 }
