fc049c8266fe3307a247b355c07e569a0a9dcdf9
Wei <wzheng100@gmail.com>
2021-07-30 15:25:44 -0700

    Part 2

--- Generics/GenericsClass/src/masterclass/udemy/Main.java
+++ Generics/GenericsClass/src/masterclass/udemy/Main.java
@@ -3,15 +3,34 @@ package masterclass.udemy;
 public class Main {
 
     public static void main(String[] args) {
-	FootballPlayer joe = new FootballPlayer("Joe");
-	BaseballPlayer pat = new BaseballPlayer("Pat");
-	SoccerPlayer beckham = new SoccerPlayer("Beckham");
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-	Team adelaidCrows = new Team("Adelaid Crows");
-	adelaidCrows.addPlayer(joe);
-	adelaidCrows.addPlayer(pat);
-	adelaidCrows.addPlayer(beckham);
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+//	adelaideCrows.addPlayer(pat);
+//	adelaideCrows.addPlayer(beckham);
 
-        System.out.println(adelaidCrows.numPlayers());
+        System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("This won't work");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordan");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle, 2, 1);
+//        adelaideCrows.matchResult(baseballTeam, 1, 1);
     }
 }
--- Generics/GenericsClass/src/masterclass/udemy/Team.java
+++ Generics/GenericsClass/src/masterclass/udemy/Team.java
@@ -1,15 +1,16 @@
 package masterclass.udemy;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Team {
+public class Team<T extends Player> {
     private String name;
     int played = 0;
     int won = 0;
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -19,13 +20,13 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    public boolean addPlayer(T player) {
         if (members.contains(player)) {
-            System.out.println(player.getName() + " is already on this team");
+            System.out.println(((Player) player).getName() + " is already on this team");
             return false;
         } else {
             members.add(player);
-            System.out.println(player.getName() + " picked for team " + this.name);
+            System.out.println(((Player) player).getName() + " picked for team " + this.name);
             return true;
         }
     }
@@ -34,16 +35,22 @@ public class Team {
         return this.members.size();
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore) {
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+
+        String message;
         if (ourScore > theirScore) {
             won++;
+            message = " beat ";
         } else if (ourScore == theirScore) {
             tied++;
+            message = " drew with ";
         } else {
             lost++;
+            message = " lost to ";
         }
         played++;
         if(opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
     }
