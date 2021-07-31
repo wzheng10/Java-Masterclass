86c42b7cd1097f172c24d582967cbdb216a4bfc7
Wei <wzheng100@gmail.com>
2021-07-31 13:35:53 -0700

    packages

---
+++ Packages/Packages/src/masterclass/udemy/Main.java
@@ -0,0 +1,14 @@
+package masterclass.udemy;
+
+import org.w3c.dom.Node;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        javafx.scene.Node node = null;
+        org.w3c.dom.Node anotherNode = null;
+
+
+    }
+}
