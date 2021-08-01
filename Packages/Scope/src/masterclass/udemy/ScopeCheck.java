b61cd052446271ebcebdba4dc5fff117c135576d
Wei <wzheng100@gmail.com>
2021-08-01 14:40:39 -0700

    Scope

---
+++ Packages/Scope/src/masterclass/udemy/ScopeCheck.java
@@ -0,0 +1,39 @@
+package masterclass.udemy;
+
+public class ScopeCheck {
+    public int publicVar = 0;
+    private int varOne = 1;
+
+    public ScopeCheck() {
+        System.out.println("ScopeCheck created, publicVar = " + publicVar + ": privateVar = " + varOne);
+    }
+
+    public int getVarOne() {
+        return varOne;
+    }
+
+    public void timesTwo() {
+        int privateVar = 2;
+        for(int i = 0; i< 10; i++) {
+            System.out.println(i + " times two is " + i * privateVar);
+//            System.out.println("Value of i is now " + i);
+        }
+    }
+
+    public class InnerClass {
+        public int privateVar = 3;
+
+        public InnerClass() {
+            System.out.println("InnerClass created, privateVar is " + privateVar);
+        }
+
+        public void timesTwo() {
+            ScopeCheck.this.timesTwo();
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i + " times two is " + i * ScopeCheck.this.varOne);
+                //ScopeCheck.this.privateVar will refer to the class scope since
+                //this.privateVar will result in an error as there are no privateVar variables within the innerClass
+            }
+        }
+    }
+}
