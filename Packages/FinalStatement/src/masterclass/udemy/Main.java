b73d7cc746b4a7c9c3199610f413308c305aea91
Wei <wzheng100@gmail.com>
2021-08-02 15:12:53 -0700

    Final Statment

---
+++ Packages/FinalStatement/src/masterclass/udemy/Main.java
@@ -0,0 +1,30 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	SomeClass one = new SomeClass("one");
+	SomeClass two = new SomeClass("two");
+	SomeClass three = new SomeClass("three");
+
+		System.out.println(one.getInstanceNumber());
+		System.out.println(two.getInstanceNumber());
+		System.out.println(three.getInstanceNumber());
+
+//		one.instanceNumber = 4;
+		System.out.println(Math.PI);
+//		Math m = new Math();
+		int pw = 123459096;
+		Password password = new Password(pw);
+		password.storePassword();
+
+		password.letMeIn(1);
+		password.letMeIn(123);
+		password.letMeIn(-1);
+		password.letMeIn(pw);
+		password.letMeIn(123456);
+
+	}
+
+
+}
---
+++ Packages/FinalStatement/src/masterclass/udemy/Password.java
@@ -0,0 +1,28 @@
+package masterclass.udemy;
+
+public class Password {
+    private static final int key = 123456;
+    private final int encryptedPassword;
+
+    public Password(int password) {
+        this.encryptedPassword = encryptDecrypt(password);
+    }
+
+    private int encryptDecrypt(int password) {
+        return password ^ key;
+    }
+
+    public void storePassword() {
+        System.out.println("Saving password as " + this.encryptedPassword);
+    }
+
+    public boolean letMeIn(int password) {
+        if(encryptDecrypt(password) == this.encryptedPassword) {
+            System.out.println("Welcome");
+            return true;
+        } else {
+            System.out.println("You cannot come in");
+            return false;
+        }
+    }
+}
