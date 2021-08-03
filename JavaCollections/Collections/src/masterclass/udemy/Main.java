35b857599ff6fee31b67af950f374f3a82d051f6
Wei <wzheng100@gmail.com>
2021-08-02 23:16:38 -0700

    Theatre Seats

---
+++ JavaCollections/Collections/src/masterclass/udemy/Main.java
@@ -0,0 +1,19 @@
+package masterclass.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Theatre theatre = new Theatre("Olympian", 8, 12);
+//	    theatre.getSeats();
+	    if(theatre.reserveSeat("H11")) {
+            System.out.println("Please Pay");
+        } else {
+            System.out.println("Sorry, but the seat is taken");
+        }
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please Pay");
+        } else {
+            System.out.println("Sorry, but the seat is taken");
+        }
+    }
+}
