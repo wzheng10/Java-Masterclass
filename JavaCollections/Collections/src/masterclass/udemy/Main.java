c71e11788bc6e2a796afe500254d033524f2e1f3
Wei <wzheng100@gmail.com>
2021-08-03 00:00:40 -0700

    Reserve

--- JavaCollections/Collections/src/masterclass/udemy/Main.java
+++ JavaCollections/Collections/src/masterclass/udemy/Main.java
@@ -9,6 +9,13 @@ public class Main {
 	    Theatre theatre = new Theatre("Olympian", 8, 12);
 	    List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
 	    printList(seatCopy);
+
+	    seatCopy.get(1).reserve();
+	    if(theatre.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
+        } else {
+            System.out.println("Seat already reserved");
+        }
 //	    theatre.getSeats();
 //	    if(theatre.reserveSeat("D12")) {
 //            System.out.println("Please Pay");
