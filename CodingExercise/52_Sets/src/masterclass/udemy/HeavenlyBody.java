75d9f3463442e5b42b9363603799f7e8eba40212
Wei <wzheng100@gmail.com>
2021-08-05 13:41:11 -0700

    Sets Bulk Operations

---
+++ CodingExercise/52_Sets/src/masterclass/udemy/HeavenlyBody.java
@@ -0,0 +1,55 @@
+package masterclass.udemy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(this.satellites);
+    }
+//  The HeavenlyBody class is declared final, so we cannot be subclassed.
+//   The Java String class is also final, which is why it can safely use the instanceof
+//   method without having to worry about comparisons with a subclass
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        System.out.println("obj.getClass() is " + obj.getClass());
+        System.out.println("this.getClass()" + this.getClass());
+        if ((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+
+        String objName = ((HeavenlyBody) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        System.out.println("HashCode Called");
+        return this.name.hashCode() + 57;
+    }
+}
