b6e9e8d49236579c6c270389eeb7e8e834df02db
Wei <wzheng100@gmail.com>
2021-08-07 14:05:40 -0700

    StockList with Maps

--- JavaCollections/SortedCollection/src/masterclass/udemy/StockItem.java
+++ JavaCollections/SortedCollection/src/masterclass/udemy/StockItem.java
@@ -25,7 +25,7 @@ public class StockItem implements Comparable<StockItem> {
         return price;
     }
 
-    public int getQuantityStock() {
+    public int quantityInStock() {
         return quantityStock;
     }
 
---
+++ JavaCollections/SortedCollection/src/masterclass/udemy/StockList.java
@@ -0,0 +1,59 @@
+package masterclass.udemy;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new HashMap<>();
+    }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            if (inStock != item) {
+                item.adjustStock(inStock.quantityInStock());
+            }
+            list.put(item.getName(), item);
+            return item.quantityInStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity) {
+        StockItem inStock = list.getOrDefault(item, null);
+        if ((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0)) {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public StockItem get(String key) {
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> Items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\nStock List\n";
+        double totalCost = 0.0;
+        for(Map.Entry<String, StockItem> item : list.entrySet()) {
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+
+            s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
+            s = s + itemValue + "\n";
+            totalCost += itemValue;
+        }
+
+        return s + "Total stock value " + totalCost;
+    }
+}
