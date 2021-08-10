129ac2f3fe4b72f0fa4da96467869a328af9563c
Wei <wzheng100@gmail.com>
2021-08-09 21:59:41 -0700

    Threads

--- JavaFX/Events/src/sample/Controller.java
+++ JavaFX/Events/src/sample/Controller.java
@@ -1,8 +1,11 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -12,6 +15,10 @@ public class Controller {
     private Button helloButton;
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
 
     @FXML
     public void initialize() {
@@ -23,11 +30,42 @@ public class Controller {
     public void onButtonClicked(ActionEvent e) {
 //        System.out.println("Hello, " + nameField.getText());
 //        System.out.println("The following button was pressed: " + e.getSource());
-        if(e.getSource().equals(helloButton)) {
+        if (e.getSource().equals(helloButton)) {
             System.out.println("Hello, " + nameField.getText());
-        } else if(e.getSource().equals(byeButton))
+        } else if (e.getSource().equals(byeButton)) {
             System.out.println("Bye, " + nameField.getText());
+        }
+
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                    System.out.println("I'm going to sleep on the : " + s);
+
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+                            System.out.println("I'm updating the label on the : " + s);
+                            ourLabel.setText("We did something");
+                        }
+                    });
+                } catch (InterruptedException event) {
+                    //we dont care about this
+                }
+            }
+        };
+        new Thread(task).start();
+
+        if (ourCheckBox.isSelected()) {
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
     }
+
 
     @FXML
     public void handleKeyReleased() {
@@ -35,5 +73,9 @@ public class Controller {
         boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
         helloButton.setDisable(disableButtons);
         byeButton.setDisable(disableButtons);
+    }
+
+    public void handleChange() {
+        System.out.println("The checkbox is " + (ourCheckBox.isSelected() ? "checked" : "not checked"));
     }
 }
