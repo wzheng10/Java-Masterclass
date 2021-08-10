74e8aa8d6818c2f8d94ca6abb79033cd96ee4b7a
Wei <wzheng100@gmail.com>
2021-08-10 04:14:05 -0700

    Todo List App

---
+++ JavaFX/ToDoList/src/masterclass/udemy/todolist/datamodel/TodoItem.java
@@ -0,0 +1,45 @@
+package masterclass.udemy.todolist.datamodel;
+
+import java.time.LocalDate;
+
+public class TodoItem {
+    private String shortDescription;
+    private String details;
+    private LocalDate deadline;
+
+    public TodoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    //creates the shortDescription on the lefthand side to be visible
+    @Override
+    public String toString() {
+        return shortDescription;
+    }
+}
---
+++ JavaFX/ToDoList/src/module-info.java
@@ -0,0 +1,6 @@
+module ToDoList {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    opens masterclass.udemy.todolist;
+}
