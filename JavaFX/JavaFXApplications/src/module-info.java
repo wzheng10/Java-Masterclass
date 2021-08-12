0c6ae12ff895e22e87aef33e6691d4da7f72c8c0
Wei <wzheng100@gmail.com>
2021-08-11 23:05:26 -0700

    WebView

--- JavaFX/JavaFXApplications/src/module-info.java
+++ JavaFX/JavaFXApplications/src/module-info.java
@@ -2,5 +2,7 @@ module JavaFXApplications {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+    requires java.desktop;
+    requires javafx.web;
     opens sample;
 }
