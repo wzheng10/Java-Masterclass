7435c2b1f2391a70f22dfd5eba4b34333d297ef0
Wei <wzheng100@gmail.com>
2021-08-12 23:56:34 -0700

    Multi Catch

--- BasicInputOutput/Exceptions/src/Example.java
+++ BasicInputOutput/Exceptions/src/Example.java
@@ -4,33 +4,36 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException| NoSuchElementException e) { // way to check for multiple exceptions
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
     }
 
     private static int divide() {
         int x, y;
-        try {
+//        try {
             x = getInt();
             y = getInt();
-        } catch(NoSuchElementException e) {     //catches all non-integers
-            throw new ArithmeticException("No suitable input");
-        }
-        System.out.println("x is " + x + ", y is " + y);
-        try {
+            System.out.println("x is " + x + ", y is " + y);
             return x / y;
-        } catch(ArithmeticException e) {    //catches the 0 attempt when dividing
-            throw new ArithmeticException("attempt to divide by 0");
-        }
+//        } catch (NoSuchElementException e) {     //catches all non-integers
+//            throw new NoSuchElementException("No suitable input");
+//        } catch (ArithmeticException e) {       //catches all 0
+//            throw new ArithmeticException("attempt to divide by 0");
+//        }
     }
 
     private static int getInt() {
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter an integer ");
-        while(true) {
+        while (true) {
             try {
                 return s.nextInt();
-            } catch(InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 //go around again. Read past the end of line in the input first
                 s.nextLine();
                 System.out.println("Enter a number: ");
