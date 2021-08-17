c30ea9c4e17824f02cefab8cdcd8ce60faab49a7
Wei <wzheng100@gmail.com>
2021-08-16 19:30:51 -0700

    quit

--- BasicInputOutput/InputOutput/src/masterclass/udemy/Location.java
+++ BasicInputOutput/InputOutput/src/masterclass/udemy/Location.java
@@ -17,9 +17,9 @@ public class Location {
         this.locationID = locationID;
         this.description = description;
         if(exits != null) {
-            this.exits = new HashMap<String, Integer>(exits);
+            this.exits = new LinkedHashMap<String, Integer>(exits);
         } else {
-            this.exits = new HashMap<String, Integer>();
+            this.exits = new LinkedHashMap<String, Integer>();
         }
         this.exits.put("Q", 0);
     }
@@ -37,7 +37,7 @@ public class Location {
     }
 
     public Map<String, Integer> getExits() {
-        return new HashMap<String, Integer>(exits);
+        return new LinkedHashMap<String, Integer>(exits);
     }
 
     protected  void addExit(String direction, int location) {
