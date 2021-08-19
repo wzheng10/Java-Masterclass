4ca24ebd23ca5436bfc66e4d7822411405e5538e
Wei <wzheng100@gmail.com>
2021-08-19 00:27:26 -0700

    Creating RAF

--- BasicInputOutput/AdventureRandom/src/IndexRecord.java
+++ BasicInputOutput/AdventureRandom/src/IndexRecord.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class IndexRecord {
+public class IndexRecord {
+    private int startByte;
+    private int length;
+
+    public IndexRecord(int startByte, int length) {
+        this.startByte = startByte;
+        this.length = length;
+    }
+
+    public int getStartByte() {
+        return startByte;
+    }
+
+    public void setStartByte(int startByte) {
+        this.startByte = startByte;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
---
+++ BasicInputOutput/AdventureRandom/src/masterclass/udemy/Locations.java
@@ -0,0 +1,123 @@
+package masterclass.udemy;
+
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.util.*;
+
+public class Locations implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
+
+    public static void main(String[] args) throws IOException {
+        try (RandomAccessFile rao = new RandomAccessFile("locations_rand.dat", "rwd")) {
+            rao.writeInt(locations.size());
+            int indexSize = locations.size() * 3 * Integer.BYTES;
+            int locationStart = (int) (indexSize + rao.getFilePointer() + Integer.BYTES);
+            rao.writeInt(locationStart);
+
+            long indexStart = rao.getFilePointer();
+
+            int startPointer = locationStart;
+            rao.seek(startPointer);
+
+            for(Location location : locations.values()) {
+                rao.writeInt(location.getLocationID());
+                rao.writeUTF(location.getDescription());
+                StringBuilder builder = new StringBuilder();
+                for(String direction : location.getExits().keySet()) {
+                    if(!direction.equalsIgnoreCase("Q")) {
+                        builder.append(direction);
+                        builder.append(",");
+                        builder.append(location.getExits().get(direction));
+                        builder.append(",");
+                    }
+                }
+            }
+        }
+    }
+
+static {
+
+        try(ObjectInputStream locFile=new ObjectInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))){
+        boolean eof=false;
+        while(!eof){
+        try{
+        Location location=(Location)locFile.readObject();
+        System.out.println("Read location "+location.getLocationID()+" : "+location.getDescription());
+        System.out.println("Found "+location.getExits().size()+" exits");
+
+        locations.put(location.getLocationID(),location);
+        }catch(EOFException e){
+        eof=true;
+        }
+        }
+
+        }catch(InvalidClassException e){
+        System.out.println("InvalidClassException "+e.getMessage());
+        }catch(IOException io){
+        System.out.println("IO Exception");
+        }catch(ClassNotFoundException e){
+        System.out.println("ClassNotFoundException "+e.getMessage());
+        }
+        }
+
+
+@Override
+public int size(){
+        return locations.size();
+        }
+
+@Override
+public boolean isEmpty(){
+        return locations.isEmpty();
+        }
+
+@Override
+public boolean containsKey(Object key){
+        return locations.containsKey(key);
+        }
+
+@Override
+public boolean containsValue(Object value){
+        return locations.containsValue(value);
+        }
+
+@Override
+public Location get(Object key){
+        return locations.get(key);
+        }
+
+@Override
+public Location put(Integer key,Location value){
+        return locations.put(key,value);
+        }
+
+@Override
+public Location remove(Object key){
+        return locations.remove(key);
+        }
+
+@Override
+public void putAll(Map<?extends Integer, ?extends Location> m){
+
+        }
+
+@Override
+public void clear(){
+        locations.clear();
+        }
+
+@Override
+public Set<Integer> keySet(){
+        return locations.keySet();
+        }
+
+@Override
+public Collection<Location> values(){
+        return locations.values();
+        }
+
+@Override
+public Set<Entry<Integer, Location>>entrySet(){
+        return locations.entrySet();
+        }
+        }
