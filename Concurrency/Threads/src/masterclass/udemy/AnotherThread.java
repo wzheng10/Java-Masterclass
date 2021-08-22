684e231780be88155cf2811ab41ca37fb69f1a74
Wei <wzheng100@gmail.com>
2021-08-21 22:01:18 -0700

    Thread Colors

---
+++ Concurrency/Threads/src/masterclass/udemy/AnotherThread.java
@@ -0,0 +1,9 @@
+package masterclass.udemy;
+
+public class AnotherThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("Hello from another thread.");
+    }
+}
--- Concurrency/Threads/src/masterclass/udemy/Main.java
+++ Concurrency/Threads/src/masterclass/udemy/Main.java
@@ -3,6 +3,19 @@ package masterclass.udemy;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello from the main thread.");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+        new Thread() {
+            public void run() {
+                System.out.println("Hello from the anonymous class thread");
+            }
+        }.start();
+
+        System.out.println("Hello again from the main thread.");
+
+
+
     }
 }
