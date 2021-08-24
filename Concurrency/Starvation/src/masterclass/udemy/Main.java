cddacac46379973d148cc9f7d3caa4a524cf79d7
Wei <wzheng100@gmail.com>
2021-08-23 17:49:21 -0700

    Fair Lock

--- Concurrency/Starvation/src/masterclass/udemy/Main.java
+++ Concurrency/Starvation/src/masterclass/udemy/Main.java
@@ -1,7 +1,10 @@
 package masterclass.udemy;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
-    private static Object lock = new Object();
+    private static ReentrantLock lock = new ReentrantLock(true);
+
     public static void main(String[] args) {
         Thread t1 = new Thread(new Worker(ThreadColor.ANSI_RED), "Priority 10");
         Thread t2 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 8");
@@ -9,11 +12,11 @@ public class Main {
         Thread t4 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 4");
         Thread t5 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 2");
 
-        t1.setPriority(10);
-        t2.setPriority(8);
-        t3.setPriority(6);
-        t4.setPriority(4);
-        t5.setPriority(2);
+//        t1.setPriority(10);
+//        t2.setPriority(8);
+//        t3.setPriority(6);
+//        t4.setPriority(4);
+//        t5.setPriority(2);
 
         t3.start();
         t2.start();
@@ -33,11 +36,15 @@ public class Main {
 
         @Override
         public void run() {
-            for(int i = 0; i < 100; i++) {
-                synchronized (lock) {
+            for (int i = 0; i < 100; i++) {
+                lock.lock();
+                try {
                     System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
                     //execute critical section code
+                } finally {
+                    lock.unlock();
                 }
+
             }
         }
     }
