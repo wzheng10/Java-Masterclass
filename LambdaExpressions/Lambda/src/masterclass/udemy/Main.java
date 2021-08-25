e4389742773659f7d782c3f9be80c81c06bc9ee4
Wei <wzheng100@gmail.com>
2021-08-25 14:10:03 -0700

    Non Lambda Block

--- LambdaExpressions/Lambda/src/masterclass/udemy/Main.java
+++ LambdaExpressions/Lambda/src/masterclass/udemy/Main.java
@@ -1,9 +1,6 @@
 package masterclass.udemy;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -51,6 +48,19 @@ public class Main {
         for(Employee employee : employees) {
             System.out.println(employee.getName());
         }
+
+        String sillyString = doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + " " +  s2.toUpperCase();
+            }
+        },
+                employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 }
 //
@@ -85,4 +95,8 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
 }
