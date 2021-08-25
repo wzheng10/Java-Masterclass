3b5c1556870e11098711c14b4173dc78e36b86ed
Wei <wzheng100@gmail.com>
2021-08-25 13:49:08 -0700

    Lambda expression

---
+++ LambdaExpressions/Lambda/src/masterclass/udemy/Main.java
@@ -0,0 +1,82 @@
+package masterclass.udemy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	new Thread(new CodeToRun()).start();
+        //anonymous class
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from the Runnable");
+//            }
+//        }).start();
+//    }
+        //lambda expression
+        new Thread(() -> {
+            System.out.println("Printing from the Runnable");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n", 3);
+        }).start();
+
+
+        Employee john = new Employee("John Doe", 30);
+        Employee wei = new Employee("Wei Zheng", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(wei);
+        employees.add(jack);
+        employees.add(snow);
+
+//        Collections.sort(employees, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee employee1, Employee employee2) {
+//                return employee1.getName().compareTo(employee2.getName());
+//            }
+//        });
+        for(Employee employee : employees) {
+            System.out.println(employee.getName());
+        }
+    }
+}
+//
+//class CodeToRun implements Runnable {
+//    @Override
+//    public void run() {
+//        System.out.println("Printing from the Runnable");
+//    }
+//}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
