50592a2d11e6e5f1a22747a4547b7b10925be37b
Wei <wzheng100@gmail.com>
2021-08-25 20:10:00 -0700

    Scope

--- LambdaExpressions/Lambda/src/masterclass/udemy/Main.java
+++ LambdaExpressions/Lambda/src/masterclass/udemy/Main.java
@@ -33,6 +33,24 @@ public class Main {
         employees.add(jack);
         employees.add(snow);
 
+        employees.forEach(employee -> {
+            System.out.println(employee.getName());
+            System.out.println(employee.getAge());
+        });
+//        for (Employee employee : employees) {
+//            System.out.println(employee.getName());
+//            System.out.println(employee.getAge());
+//        }
+    }
+
+//        System.out.println("*********************************************");
+//
+//        for(int i=0; i<employees.size(); i++) {
+//            Employee employee = employees.get(i);
+//            System.out.println(employee.getName());
+//            new Thread(() -> System.out.println(employee.getAge())).start();
+//        }
+
 //        Collections.sort(employees, new Comparator<Employee>() {
 //            @Override
 //            public int compare(Employee employee1, Employee employee2) {
@@ -66,10 +84,9 @@ public class Main {
 //        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
 //        System.out.println(sillyString);
 
-        AnotherClass anotherClass = new AnotherClass();
-        String s = anotherClass.doSomething();
-        System.out.println(s);
-    }
+//        AnotherClass anotherClass = new AnotherClass();
+//        String s = anotherClass.doSomething();
+//        System.out.println(s);
 
     public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
         return uc.upperAndConcat(s1, s2);
@@ -115,7 +132,6 @@ interface UpperConcat {
 
 class AnotherClass {
     public String doSomething() {
-        int i = 0;
 
         UpperConcat uc = (s1, s2) -> {
             System.out.println("The lambda expression's class is " + getClass().getSimpleName());
