c400a3c225b4926709513880e698c41ea0888bd3
Wei <wzheng100@gmail.com>
2021-08-25 23:20:36 -0700

    Functions

--- LambdaExpressions/Employees/src/masterclass/udemy/Main.java
+++ LambdaExpressions/Employees/src/masterclass/udemy/Main.java
@@ -3,6 +3,7 @@ package masterclass.udemy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -25,33 +26,60 @@ public class Main {
         employees.add(red);
         employees.add(charming);
 
+        Function<Employee, String> getLastName = (Employee employee) -> {
+            return employee.getName().substring(employee.getName().indexOf(' ') + 1);
+        };
 
-        printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
-        printEmployeesByAge(employees, "\nEmployees 30 and under", employee -> employee.getAge() <= 30);
-        printEmployeesByAge(employees, "\nEmployees younger than 25", new Predicate<Employee>() {
-            @Override
-            public boolean test(Employee employee) {
-                return employee.getAge() < 25;
+        String lastName = getLastName.apply(employees.get(1));
+//        System.out.println(lastName);
+
+        Function<Employee, String> getFirstName = (Employee employee) -> {
+            return employee.getName().substring(0, employee.getName().indexOf(' '));
+        };
+
+        Random random1 = new Random();
+        for (Employee employee : employees) {
+            if (random1.nextBoolean()) {
+                System.out.println(getAName(getFirstName, employee));
+            } else {
+                System.out.println(getAName(getLastName, employee));
             }
-        });
-
-        IntPredicate greaterThan15 = i -> i > 15;
-        IntPredicate lessThan100 = i -> i < 100;
-
-        System.out.println(greaterThan15.test(10));
-        int a = 20;
-        System.out.println(greaterThan15.test(a + 5));
-
-        System.out.println(greaterThan15.and(lessThan100).test(50));
-        System.out.println(greaterThan15.and(lessThan100).test(15));
-
-        Random random = new Random();
-        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
-        for(int i=0; i<10; i++) {
-            System.out.println(randomSupplier.get());
         }
-
     }
+        private static String getAName(Function < Employee, String > getName, Employee employee){
+            return getName.apply(employee);
+        }
+//        printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
+//        printEmployeesByAge(employees, "\nEmployees 30 and under", employee -> employee.getAge() <= 30);
+//        printEmployeesByAge(employees, "\nEmployees younger than 25", new Predicate<Employee>() {
+//            @Override
+//            public boolean test(Employee employee) {
+//                return employee.getAge() < 25;
+//            }
+//        });
+//
+//        IntPredicate greaterThan15 = i -> i > 15;
+//        IntPredicate lessThan100 = i -> i < 100;
+//
+//        System.out.println(greaterThan15.test(10));
+//        int a = 20;
+//        System.out.println(greaterThan15.test(a + 5));
+//
+//        System.out.println(greaterThan15.and(lessThan100).test(50));
+//        System.out.println(greaterThan15.and(lessThan100).test(15));
+//
+//        Random random = new Random();
+//        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+//        for(int i=0; i<10; i++) {
+//            System.out.println(randomSupplier.get());
+//        }
+//
+//        employees.forEach(employee-> {
+//            String lastName = employee.getName().substring(employee.getName().indexOf(' ') + 1);
+//            System.out.println("Last Name is: " + lastName);
+//        });
+
+//    }
 //        System.out.println("\nEmployees 30 and younger:");
 //        System.out.println("===========================");
 //        employees.forEach(employee -> {
@@ -60,7 +88,7 @@ public class Main {
 //           }
 //        });
 
-        //Enhanced for loop
+    //Enhanced for loop
 //        for(Employee employee: employees) {
 //            if (employee.getAge() > 30) {
 //
