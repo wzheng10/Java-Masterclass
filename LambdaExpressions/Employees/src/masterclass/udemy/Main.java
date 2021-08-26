7c6a3d127f739649a56a666a4b421b6451de688a
Wei <wzheng100@gmail.com>
2021-08-25 22:55:04 -0700

    Predicate/Supplier

--- LambdaExpressions/Employees/src/masterclass/udemy/Main.java
+++ LambdaExpressions/Employees/src/masterclass/udemy/Main.java
@@ -2,13 +2,16 @@ package masterclass.udemy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
         Employee john = new Employee("John Doe", 30);
-        Employee wei = new Employee("Wei Zheng", 25);
+        Employee wei = new Employee("Wei Zheng", 24);
         Employee jack = new Employee("Jack Hill", 40);
         Employee snow = new Employee("Snow White", 22);
         Employee red = new Employee("Red RidingHood", 35);
@@ -25,7 +28,30 @@ public class Main {
 
         printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
         printEmployeesByAge(employees, "\nEmployees 30 and under", employee -> employee.getAge() <= 30);
+        printEmployeesByAge(employees, "\nEmployees younger than 25", new Predicate<Employee>() {
+            @Override
+            public boolean test(Employee employee) {
+                return employee.getAge() < 25;
+            }
+        });
 
+        IntPredicate greaterThan15 = i -> i > 15;
+        IntPredicate lessThan100 = i -> i < 100;
+
+        System.out.println(greaterThan15.test(10));
+        int a = 20;
+        System.out.println(greaterThan15.test(a + 5));
+
+        System.out.println(greaterThan15.and(lessThan100).test(50));
+        System.out.println(greaterThan15.and(lessThan100).test(15));
+
+        Random random = new Random();
+        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+        for(int i=0; i<10; i++) {
+            System.out.println(randomSupplier.get());
+        }
+
+    }
 //        System.out.println("\nEmployees 30 and younger:");
 //        System.out.println("===========================");
 //        employees.forEach(employee -> {
@@ -41,7 +67,6 @@ public class Main {
 //                System.out.println(employee.getName());
 //            }
 //        }
-    }
 
     private static void printEmployeesByAge(List<Employee> employees,
                                             String ageText,
@@ -49,7 +74,7 @@ public class Main {
         System.out.println(ageText);
         System.out.println("===================");
         for (Employee employee : employees) {
-            if (employee.getAge() > 30) {
+            if (ageCondition.test(employee)) {
                 System.out.println(employee.getName());
             }
         }
