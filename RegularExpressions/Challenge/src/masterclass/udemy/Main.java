18bcd7d7f392ed863fea9dd1b32bfcb4b3c8846c
Wei <wzheng100@gmail.com>
2021-08-28 17:18:18 -0700

    Challenges

--- RegularExpressions/Challenge/src/masterclass/udemy/Main.java
+++ RegularExpressions/Challenge/src/masterclass/udemy/Main.java
@@ -40,7 +40,40 @@ public class Main {
         System.out.println(challenge7.matches("^[A-Z][a-z]+\\.\\d+$"));
 //        System.out.println(challenge7b.matches("[a-z]+\\.[0-9]+"));
 
+
+        String challenge8 = "abcd.135uvqz.7tzik.999";
+        Pattern pattern8 = Pattern.compile("[A-Za-z]+\\.(\\d+)");
+        Matcher matcher8 = pattern8.matcher(challenge8);
+        while (matcher8.find()) {
+            System.out.println(matcher8.group(1));
+
+        }
+
+        System.out.println("-----------------------------");
+
+        String challenge9 = "abcd.135\tuvqz.7\ttzik.999\n";
+        Pattern pattern9 = Pattern.compile("[A-Za-z]+\\.(\\d+)\\s");
+        Matcher matcher9 = pattern9.matcher(challenge9);
+        while (matcher9.find()) {
+            System.out.println(matcher9.group(1));
+
+        }
+        String challenge10 = "abcd.135\tuvqz.7\ttzik.999\n";
+        Pattern pattern10 = Pattern.compile("[A-Za-z]+\\.(\\d+)\\s");
+        Matcher matcher10 = pattern10.matcher(challenge10);
+        while (matcher10.find()) {
+            System.out.println("start = " + matcher10.start(1) + " end = " + (matcher10.end(1)));
+
+        }
+
+        String challenge11 = "{0, 2}, {0, 5}, {1,3}, {2,4}";
+        Pattern pattern11 = Pattern.compile("\\{(.+?)\\}");
+        Matcher matcher11 = pattern11.matcher(challenge11);
+        while(matcher11.find()) {
+            System.out.println("Occurence: " + matcher11.group(1));
+        }
+
+
+
     }
-
-
 }
