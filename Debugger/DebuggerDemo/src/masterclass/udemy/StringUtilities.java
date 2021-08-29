0591d8a817b673a9956a3828fb46de8b36e6004c
Wei <wzheng100@gmail.com>
2021-08-29 13:51:44 -0700

    Debugging Intro

---
+++ Debugger/DebuggerDemo/src/masterclass/udemy/StringUtilities.java
@@ -0,0 +1,11 @@
+package masterclass.udemy;
+
+public class StringUtilities {
+    private StringBuilder sBuilder = new StringBuilder();
+    private int charsAdded = 0;
+
+    public void addChar(StringBuilder sBuilder, char c) {
+        sBuilder.append(c);
+        charsAdded++;
+    }
+}
