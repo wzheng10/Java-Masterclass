38770f1e61ca2f885d84db88cb2400a826340f04
Wei <wzheng100@gmail.com>
2021-08-30 13:01:53 -0700

    Unit Testing JUnit

---
+++ Debugger/Banking/src/masterclass/udemy/BankAccountTest.java
@@ -0,0 +1,23 @@
+package masterclass.udemy;
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+        fail("This test has yet to be implemented");
+
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+        fail("This test has yet to be implemented");
+
+    }
+}
