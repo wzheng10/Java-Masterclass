39a08a4c142f04ec9b30365907124a3684864770
Wei <wzheng100@gmail.com>
2021-08-30 13:46:31 -0700

    Assert

--- Debugger/Banking/src/masterclass/udemy/BankAccountTest.java
+++ Debugger/Banking/src/masterclass/udemy/BankAccountTest.java
@@ -6,7 +6,9 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Wei", "Zheng", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
     }
 
     @org.junit.Test
@@ -16,8 +18,22 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void getBalance() {
-        fail("This test has yet to be implemented");
+    public void getBalance_deposit() {
+        BankAccount account = new BankAccount("Wei", "Zheng", 1000.00,  BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
 
+    }
+    @org.junit.Test
+    public void getBalance_withdrawal() {
+        BankAccount account = new BankAccount("Wei", "Zheng", 1000.00,  BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Wei", "Zheng", 1000.00,  BankAccount.SAVINGS);
+        assertTrue("The account is NOT a checking account",account.isChecking());
     }
 }
