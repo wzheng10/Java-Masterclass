1ceb37161fa49d25bdfc294ba528b261bca1de4a
Wei <wzheng100@gmail.com>
2021-08-30 18:35:55 -0700

    Parameterized

--- Debugger/Banking/src/masterclass/udemy/BankAccountTest.java
+++ Debugger/Banking/src/masterclass/udemy/BankAccountTest.java
@@ -29,18 +29,27 @@ public class BankAccountTest {
         assertEquals(400.00, balance, 0);
     }
 
-    @org.junit.Test(expected = IllegalArgumentException.class)
+    @org.junit.Test (expected = IllegalArgumentException.class)
     public void withdrawal_notBranch() throws Exception {
         double balance = account.withdraw(600.00, false);
-        assertEquals(400.00, balance, 0);
+        fail("Should have thrown IllegalArgumentException");
     }
+    //old way
+//    @org.junit.Test
+//    public void withdrawal_notBranch() throws Exception {
+//        try {
+//            account.withdraw(600.00, true);
+//            fail("Should have thrown IllegalArgumentException");
+//        } catch (IllegalArgumentException e) {
+//        }
+//    }
 
     @org.junit.Test
     public void getBalance_deposit() throws Exception {
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
-
     }
+
     @org.junit.Test
     public void getBalance_withdrawal() throws Exception {
         account.withdraw(200.00, true);
