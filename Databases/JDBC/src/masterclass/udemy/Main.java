ce11f73de9a9c40c0464c6b9ede26ed68652583a
Wei <wzheng100@gmail.com>
2021-09-02 23:57:51 -0700

    Connecting to db

--- Databases/JDBC/src/masterclass/udemy/Main.java
+++ Databases/JDBC/src/masterclass/udemy/Main.java
@@ -1,8 +1,17 @@
 package masterclass.udemy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\databases\\testjabv.db") ;
+        }catch(SQLException e) {
+            System.out.println("Something went wrong" + e.getMessage());
+        }
     }
 }
