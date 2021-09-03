198398fe1103536d9d9955a35c91a17499786ea5
Wei <wzheng100@gmail.com>
2021-09-03 13:57:16 -0700

    Creating db with JDBC

--- Databases/JDBC/src/masterclass/udemy/Main.java
+++ Databases/JDBC/src/masterclass/udemy/Main.java
@@ -3,15 +3,26 @@ package masterclass.udemy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\databases\\testjabv.db") ;
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Java\\Java-Masterclass\\Databases\\JDBC\\testjava.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+
+            //*RECOMMENDED CONNECTION*
+//      try (Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Java\\Java-Masterclass\\Databases\\JDBC\\testjava.db");
+//           Statement statement = conn.createStatement()) {
+//           statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.close();
+            conn.close();
+
         }catch(SQLException e) {
-            System.out.println("Something went wrong" + e.getMessage());
+            System.out.println("Something went wrong: " + e.getMessage());
         }
     }
 }
