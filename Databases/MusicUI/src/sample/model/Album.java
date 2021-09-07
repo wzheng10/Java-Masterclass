5c887b2e91d3bdf1ca5f5674e29f53bd6c52cd88
Wei <wzheng100@gmail.com>
2021-09-06 23:34:18 -0700

    JavaFX Music JDBC Setup

---
+++ Databases/MusicUI/src/sample/model/Album.java
@@ -0,0 +1,32 @@
+package masterclass.udemy.model;
+
+public class Album {
+
+    private int id;
+    private String name;
+    private int artistID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getArtistID() {
+        return artistID;
+    }
+
+    public void setArtistID(int artistID) {
+        this.artistID = artistID;
+    }
+}
