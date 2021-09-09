5e461abbfb00917694f059dfcc9272f1c07c610f
Wei <wzheng100@gmail.com>
2021-09-08 17:27:06 -0700

    Multi Threaded

---
+++ NetworkProgramming/EchoServer/src/masterclass/udemy/Echoer.java
@@ -0,0 +1,41 @@
+package masterclass.udemy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Echoer extends Thread{
+    private Socket socket;
+
+    public Echoer(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            while(true) {
+                String echoString = input.readLine();
+                if(echoString.equals("exit")) {
+                    break;
+                }
+
+                output.println(echoString);
+            }
+        } catch(IOException e) {
+            System.out.println("OOPS: " + e.getMessage());
+        } finally {
+            try {
+                socket.close();
+            } catch(IOException e) {
+
+            }
+        }
+    }
+}
--- NetworkProgramming/EchoServer/src/masterclass/udemy/Main.java
+++ NetworkProgramming/EchoServer/src/masterclass/udemy/Main.java
@@ -1,28 +1,21 @@
 package masterclass.udemy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 
 public class Main {
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client Connected");
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while(true) {
-                String echoString = input.readLine();
-                if (echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
+                //Does the same as the 3 lines below
+                new Echoer(serverSocket.accept()).start();
+//                Socket socket = serverSocket.accept();
+//                Echoer echoer = new Echoer(socket);
+//                echoer.start();
+
             }
         } catch (IOException e) {
             System.out.println("Server exception " + e.getMessage());
