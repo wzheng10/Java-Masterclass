dbd239f4c959d0f9c6337ad4140d509811db7eee
Wei <wzheng100@gmail.com>
2021-09-08 22:31:24 -0700

    HttpURLConnection

--- NetworkProgramming/Networking/src/masterclass/udemy/Main.java
+++ NetworkProgramming/Networking/src/masterclass/udemy/Main.java
@@ -4,62 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org/");
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
 
-            BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream()));
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
 
-            String line = "";
-            while(line != null) {
-                line = inputStream.readLine();
+            if(responseCode != 200) {
+                System.out.println("Error reading web page");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            while ((line = inputReader.readLine()) != null){
                 System.out.println(line);
             }
-            inputStream.close();
+            inputReader.close();
 
-//            URI uri = url.toURI();
-
-//            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-//            URI baseUri = new URI("http://username:password@mynewserver.com:5000");
-//            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
-//            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
-//            URI uri3 = new URI("/stores/locatioins?zip=12345");
-//
-//            URI resolvedUri1 = baseUri.resolve(uri1);
-//            URI resolvedUri2 = baseUri.resolve(uri2);
-//            URI resolvedUri3 = baseUri.resolve(uri3);
-//
-//            URL url1 = resolvedUri1.toURL();
-//            System.out.println("URL = " + url1);
-//            URL url2 = resolvedUri2.toURL();
-//            System.out.println("URL = " + url2);
-//            URL url3 = resolvedUri3.toURL();
-//            System.out.println("URL = " + url3);
-//
-//            URI relativizedURI = baseUri.relativize(resolvedUri2);
-//            System.out.println("Relative URI = " + relativizedURI);
-
-//            URI uri = new URI("hello");
-
-//            System.out.println("Scheme = " + uri.getScheme());
-//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-//            System.out.println("Authority = " + uri.getAuthority());
-//            System.out.println("User info = " +uri.getUserInfo());
-//            System.out.println("Host = " + uri.getHost());
-//            System.out.println("Port = " + uri.getPort());
-//            System.out.println("Path = " + uri.getPath());
-//            System.out.println("Query = " + uri.getQuery());
-//            System.out.println("Fragment = " + uri.getFragment());
-        } catch(MalformedURLException e) {
-            System.out.println("URL Malformed: " + e.getMessage());
-        } catch(IOException e) {
+//        } catch(MalformedURLException e) {
+//            System.out.println("URL Malformed: " + e.getMessage());
+        } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
 //        } catch(URISyntaxException e) {
 //            System.out.println("URI Bad Syntax: " + e.getMessage());
